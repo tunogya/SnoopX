1fe52593278abfe399bcc3699154129fb69c8006
tunogya <20251942+tunogya@users.noreply.github.com>
2024-05-21 14:48:28 +0800

    update

--- app/(portal)/x/page.tsx
+++ app/(portal)/x/page.tsx
@@ -2,6 +2,8 @@
 
 const client_id = "eWp3dG1tVjRpM1lBWDJVSXlhR3c6MTpjaQ"
 const redirect_uri = "https://snoopx.abandon.ai/api/x/callback"
+const scope = "tweet.read%20users.read%20offline.access"
+
 
 const Page = () => {
 
@@ -9,7 +11,7 @@ const Page = () => {
     <div className={"p-4"}>
       <a
         target={"_blank"}
-        href={`https://twitter.com/i/oauth2/authorize?response_type=code&client_id=${client_id}&redirect_uri=https://snoopx.abandon.ai/api/x/callback&scope=tweet.read%20users.read%20offline.access&state=state&code_challenge=challenge&code_challenge_method=plain`}
+        href={`https://twitter.com/i/oauth2/authorize?response_type=code&client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scope}&state=state&code_challenge=challenge&code_challenge_method=plain`}
         className={"bg-gray-200 px-3 py-1.5 text-sm font-semibold"}
       >
         Sign in with X
