ee2a87d6602bec6b662093929e667acc12bab4a7
tunogya <20251942+tunogya@users.noreply.github.com>
2024-05-21 15:07:11 +0800

    update

--- app/api/x/callback/route.ts
+++ app/api/x/callback/route.ts
@@ -1,9 +1,35 @@
 import {NextRequest} from "next/server";
 
+/**
+ * Document
+ * https://developer.x.com/en/docs/authentication/oauth-2-0/user-access-token
+ */
 const GET = async (req: NextRequest) => {
-  // https://snoopx.abandon.ai/api/x/callback?state=state&code=bFFEQTR2ZUw0RWp1bHU1MGd4Vmxob0hvNWY2NDhnbEIwMXZyZnRJVnVBZEdEOjE3MTYyNzQ1MTc2NjI6MTowOmFjOjE
   const code = req.nextUrl.searchParams.get('code');
-  console.log("code", code);
+  if (!code) {
+    return Response.json({
+      error: "No code"
+    }, {
+      status: 400,
+    })
+  }
+
+  const result = await fetch(`https://api.twitter.com/2/oauth2/token`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: new URLSearchParams({
+      code: code,
+      grant_type: 'authorization_code',
+      client_id: 'rG9n6402A3dbUJKzXTNX4oWHJ',
+      redirect_uri: 'https://snoopx.abandon.ai/api/x/callback',
+      code_verifier: 'challenge'
+    })
+  }).then((res) => res.json());
+
+  console.log("result", result);
+
   return Response.json({
     ok: true,
   })
