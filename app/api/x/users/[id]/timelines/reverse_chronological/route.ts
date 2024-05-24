6357698db99150811774a7e1360c2047ea7ff46a
tunogya <20251942+tunogya@users.noreply.github.com>
2024-05-24 15:21:59 +0800

    update

---
+++ app/api/x/users/[id]/timelines/reverse_chronological/route.ts
@@ -0,0 +1,92 @@
+import {NextRequest} from "next/server";
+import {connectToDatabase} from "@/utils/mongodb";
+
+const clientId = process.env.NEXT_PUBLIC_X_CLIENT_ID || "";
+const clientSecret = process.env.X_CLIENT_SECRET || "";
+
+const GET = async (req: NextRequest, { params }: { params: { id: string } }) => {
+  const { db } = await connectToDatabase();
+  const user = await db.collection("users").findOne({
+    id: params.id,
+  })
+  if (!user) {
+    return Response.json({
+      error: "User not found",
+    }, {
+      status: 404,
+    })
+  }
+
+  const {access_token, refresh_token} = user;
+
+  const credentials = `${clientId}:${clientSecret}`;
+
+  let timelines = [];
+  try {
+    timelines = await fetch(`https://api.twitter.com/2/users/${params.id}/timelines/reverse_chronological?max_results=100`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+        "Authorization": `Bearer ${access_token}`
+      },
+    }).then((res) => res.json())
+      .then((res) => res.data);
+  } catch (e) {
+    // use refresh_token to get new access_token
+    const requestToken = await fetch(`https://api.twitter.com/2/oauth2/token`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+        "Authorization": `Basic ${Buffer.from(credentials).toString('base64')}`
+      },
+      body: new URLSearchParams({
+        grant_type: "refresh_token",
+        refresh_token: refresh_token,
+      }),
+    }).then((res) => res.json());
+    const new_access_token = requestToken.requestToken
+
+    // update access_token
+    await db.collection("users").updateOne({
+      id: params.id
+    }, {
+      $set: {
+        access_token: new_access_token
+      }
+    })
+
+    // use access_token to get timelines
+    timelines = await fetch(`https://api.twitter.com/2/users/${params.id}/timelines/reverse_chronological?max_results=100`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+        "Authorization": `Bearer ${new_access_token}`
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => res.data);
+  }
+
+  await db.collection("tweets").insertMany(timelines.map((item: any) => ({
+    id: item.id,
+    text: item.text,
+  })), {
+    ordered: false,
+  });
+
+  return Response.json({
+    success: true,
+  }, {
+    headers: {
+      'Content-Type': 'application/json',
+      'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
+      'Pragma': 'no-cache',
+      'Expires': '0',
+      'Surrogate-Control': 'no-store'
+    }
+  })
+}
+
+export {
+  GET
+}
