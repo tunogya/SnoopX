1f5ad0f188f5e1ec554084b610095d3b711532c7
tunogya <tunogya@qq.com>
2024-09-25 21:36:03 +0800

    update

--- app/(portal)/news/page.tsx
+++ app/(portal)/news/page.tsx
@@ -3,7 +3,19 @@
 const Page = () => {
   return (
     <div className={"p-4"}>
-      News
+      <div className={"relative bg-red-500"}>
+        <input
+          placeholder={"Search something..."}
+          className={"border w-full h-10 px-2"}
+        />
+        <div className={"absolute top-0 right-0 h-10 flex items-center px-2"}>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+               stroke="currentColor" className="size-6">
+            <path stroke-linecap="round" stroke-linejoin="round"
+                  d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"/>
+          </svg>
+        </div>
+      </div>
     </div>
   )
 }
