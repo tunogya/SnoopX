6ebd89b6f91bb03501c156e78d27ea081ddc5bf4
tunogya <tunogya@qq.com>
2024-09-28 03:00:35 +0800

    update

--- app/(tabs)/news/page.tsx
+++ app/(tabs)/news/page.tsx
@@ -57,13 +57,13 @@ const Page = () => {
         <div>BNB</div>
       </div>
       <div className="py-1.5 border-b">
-        <PinnedFeed title={"The last 2 times this signal flashed, #Bitcoin pumped. Will history repeat?"} author={"GuncelKriptoCom"} commentCount={11} />
+        <PinnedFeed title={"The last 2 times this signal flashed, #Bitcoin pumped. Will history repeat?"} author={"GuncelKriptoCom"} commentCount={11} readed={true} />
         <PinnedFeed title={"UPDATE: #Bitcoin is having its best September on record....on TG = t.me/first1bitcoin/1378"} author={"GuncelKriptoCom"} commentCount={11} />
         <PinnedFeed title={`As per CryptoQuant BTC$BTC ATH is here 🚀
 #Bitcoin at the $66.3K level, the last cohort of STH holding coins for 3-6 months remains.
-After that, the path to testing the ATH will be open.`} author={"GuncelKriptoCom"} commentCount={11} />
-        <PinnedFeed title={"The last 2 times this signal flashed, #Bitcoin pumped. Will history repeat?"} author={"GuncelKriptoCom"} commentCount={11} />
-        <PinnedFeed title={"UPDATE: #Bitcoin is having its best September on record....on TG = t.me/first1bitcoin/1378"} author={"GuncelKriptoCom"} commentCount={11} />
+After that, the path to testing the ATH will be open.`} author={"GuncelKriptoCom"} commentCount={11} readed={false} />
+        <PinnedFeed title={"The last 2 times this signal flashed, #Bitcoin pumped. Will history repeat?"} author={"GuncelKriptoCom"} commentCount={11} readed={true} />
+        <PinnedFeed title={"UPDATE: #Bitcoin is having its best September on record....on TG = t.me/first1bitcoin/1378"} author={"GuncelKriptoCom"} commentCount={11} readed={false} />
       </div>
       <div>
         <UserFeed title={`Bitcoin is having its best September so far.
@@ -82,17 +82,17 @@ Bears are in disbelief.`} author={"Lark Davis"} avatar={"https://s3.coinmarketca
   )
 }
 
-const PinnedFeed = ({ title, author, commentCount }: { title: string; author: string; commentCount: number }) => {
+const PinnedFeed = ({ title, author, commentCount, readed = false }: { title: string; author: string; commentCount: number, readed?: boolean }) => {
   return (
     <div className={"px-4 py-0.5"}>
-      <div className={"text-[17.64px] leading-[24px] break-words line-clamp-1"}>
+      <div className={`text-[17.64px] leading-[24px] break-words line-clamp-1 ${readed ? "text-[#A1A3A6]" : "text-black"}`}>
         {title}
       </div>
       <div className={"flex flex-row space-x-1.5"}>
-        <div className={"text-[9px] text-[#999999]"}>
+        <div className={"text-[9px] text-[#A1A3A6]"}>
           @{author}
         </div>
-        <div className={"text-[9px] text-[#999999]"}>
+        <div className={"text-[9px] text-[#A1A3A6]"}>
           {commentCount} comments
         </div>
       </div>
@@ -152,9 +152,12 @@ const UserFeed = ({ title, author, avatar, timestamp }: { title: string; author:
               20
             </div>
           </button>
-          <button className="flex items-center space-x-2">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-5">
+          <button className="flex items-center space-x-2 text-[#FF403A]">
+            {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-5">
               <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+            </svg> */}
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-5">
+              <path d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
             </svg>
             <div className="text-[12px]">
               30
