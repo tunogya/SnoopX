94101ed5141e9d88341dd1275927a31738a1001d
tunogya <tunogya@qq.com>
2024-09-28 00:38:42 +0800

    update

--- app/(tabs)/news/page.tsx
+++ app/(tabs)/news/page.tsx
@@ -30,11 +30,11 @@ const Page = () => {
         </Link>
         <div className="h-10 flex flex-col items-center justify-center">
           <div className="text-white">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-8">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
               <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 9a.75.75 0 0 0-1.5 0v2.25H9a.75.75 0 0 0 0 1.5h2.25V15a.75.75 0 0 0 1.5 0v-2.25H15a.75.75 0 0 0 0-1.5h-2.25V9Z" clip-rule="evenodd" />
             </svg>
           </div>
-          <div className="text-[8px] text-white font-medium">
+          <div className="text-[10px] text-white font-medium">
             POST
           </div>
         </div>
