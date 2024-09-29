483876b9a19886cd9b640ab4993283ecf4d7952d
tunogya <tunogya@qq.com>
2024-09-29 17:07:00 +0800

    update

---
+++ app/(tabs)/friends/page.tsx
@@ -0,0 +1,20 @@
+const Page = () => {
+    return (
+        <div className={""}>
+            <div className={"text-3xl font-medium px-6 py-4 text-center"}>
+                Invite frens
+                <br />
+                and get more SNO
+            </div>
+            <div className={"h-80"}></div>
+            <div className={"flex flex-col items-center space-y-1 px-6"}>
+                <div className="text-[#7F7F7F] text-center text-sm">Tap on the button to invite your friends</div>
+                <div className="text-white bg-[#FF403A] px-4 h-10 rounded-lg flex items-center flex-shrink-0 justify-center w-full">
+                    Invite frens
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Page
