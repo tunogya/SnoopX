37d6b967889ed37072bc22cdc0f5a17b1347de79
tunogya <tunogya@qq.com>
2024-09-30 22:39:22 +0800

    update

--- app/api/req/route.ts
+++ app/api/req/route.ts
@@ -1,11 +1,9 @@
-// /api/event/route.ts
+// /api/req/route.ts
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(req: NextRequest) {
     const event = await req.json();
     console.log(event);
-    // 检查事件是否合格，如果合格，则传入AWS SNS
-    // 否则报错返回
-
+    // 查询数据库，并返回;
     return NextResponse.json({ event });
 }
