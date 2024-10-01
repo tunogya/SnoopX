c9d0530401da8b8f21620fddf6b798d9ef01d72c
tunogya <tunogya@qq.com>
2024-10-01 22:37:49 +0800

    update

--- app/api/req/route.ts
+++ app/api/req/route.ts
@@ -1,9 +1,41 @@
 // /api/req/route.ts
 import { NextRequest, NextResponse } from 'next/server';
+import { connectToDatabase } from '../../../utils/db';
 
 export async function POST(req: NextRequest) {
-    const event = await req.json();
-    console.log(event);
-    // 查询数据库，并返回;
-    return NextResponse.json({ event });
+    try {
+        const filter = await req.json();
+        const { db } = await connectToDatabase();
+
+        const { ids, authors, kinds, limit, search, since, until } = filter;
+        const query: Record<string, any> = {};
+        if (ids && ids.length > 0) {
+            query.id = { $in: ids };
+        }
+        if (authors && authors.length > 0) {
+            query.pubkey = { $in: authors };
+        }
+        if (kinds && kinds.length > 0) {
+            query.kind = { $in: kinds };
+        }
+        if (since) {
+            query.created_at = { $gte: since };
+        }
+        if (until) {
+            query.created_at = {
+                ...query.created_at,
+                $lte: until,
+            };
+        }
+        // 查询数据库，并返回;
+        const data = db
+            .collection("events")
+            .find(query)
+            .limit(limit || 20)
+            .toArray();
+
+        return NextResponse.json(["OK", true, data]);
+    } catch (e) {
+        return NextResponse.json(["OK", false, `error: ${e}`]);
+    }
 }
