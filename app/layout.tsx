7aa9ba8e9aefb46c4990d9d597993b17a69e9b04
tunogya <tunogya@qq.com>
2024-10-02 23:33:06 +0800

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,14 +3,21 @@ import {ReactNode} from "react";
 import { Inter } from 'next/font/google';
 import Script from 'next/script';
 import './styles/globals.css';
+import type { Viewport } from 'next'
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: "SnoopX",
   description: "",
-  viewport: "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no",
 };
+ 
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+}
 
 export default function RootLayout({
                                      children,
