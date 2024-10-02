52abe81510f3bf5d17b2e2c78720b54018b3e64c
tunogya <tunogya@qq.com>
2024-10-02 23:52:35 +0800

    update

--- app/edit/page.tsx
+++ app/edit/page.tsx
@@ -1,22 +1,67 @@
 "use client";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { finalizeEvent } from "nostr-tools/pure";
+import { hexToBytes } from '@noble/hashes/utils';
+import { generateSecretKey } from "nostr-tools/pure";
+
 const Page = () => {
     const router = useRouter();
+    const [content, setContent] = useState("");
+    const [loading, setLoading] = useState(false);
+
+    const save = async () => {
+        try {
+            setLoading(true);
+            let sk;
+            if (window?.Telegram?.WebApp?.initData) {
+                const secretKey = await window.Telegram.WebApp.CloudStorage.getItem('skHex');
+                sk = hexToBytes(secretKey);
+            } else {
+                console.log("No Telegram WebApp");
+                sk = generateSecretKey();
+            }
+            console.log(sk);
+            const event = finalizeEvent({
+                kind: 1,
+                content: content,
+                tags: [],
+                created_at: Math.floor(Date.now() / 1000),
+            }, sk);
+            console.log(event);
+            const res = await fetch("/api/event", {
+                method: "POST",
+                body: JSON.stringify(event),
+            }).then(res => res.json());
+            console.log(res);
+            router.back();
+        } catch (e) {
+            console.log(e);
+        } finally {
+            setLoading(false);
+        }
+    }
+
     return (
         <div className="flex flex-col h-full">
             <div className="flex flex-row justify-between border-b border-[#F4F5F7] h-[50px] pt-1">
-                <div
+                <button
                     className="h-full px-4 py-2"
                     onClick={() => router.back()}
-                >Cancel</div>
-                <div
-                    className="text-[#A2A3A7] font-medium px-4 py-2"
-                    onClick={() => console.log("post")}
-                >Post</div>
+                >Cancel</button>
+                <button
+                    className={`font-medium px-4 py-2 ${loading || content.length === 0 ? "text-[#A2A3A7] " : ""}`}
+                    disabled={loading || content.length === 0}
+                    onClick={save}
+                >
+                    {loading ? "Posting..." : "Post"}
+                </button>
             </div>
             <textarea
                 placeholder="Talk something..."
                 autoFocus
+                value={content}
+                onChange={(e) => setContent(e.target.value)}
                 className="w-full p-4 h-full focus:outline-none placeholder:text-[#A2A3A7]" />
         </div>
     )
