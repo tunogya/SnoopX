fcacbd9f0ecf74de74b5262b757885ed0e7aa7ff
tunogya <tunogya@qq.com>
2024-10-07 18:13:33 +0800

    update

--- app/(tabs)/friends/page.tsx
+++ app/(tabs)/friends/page.tsx
@@ -8,6 +8,15 @@ const Page = () => {
         }
     }, []);
 
+    const handleInvite = () => {
+        const url = "https://t.me/snoopx_news_bot?startapp=friends";
+        if (window.Telegram.WebApp.initData) {
+            window.Telegram.WebApp.share(url);
+        } else {
+            window.location.href = url;
+        }
+    }
+
     return (
         <div className="">
             <div className={"text-3xl font-medium px-6 py-4 text-center"}>
@@ -18,9 +27,12 @@ const Page = () => {
             <div className={"h-80"}></div>
             <div className={"flex flex-col items-center space-y-1 px-6"}>
                 <div className="text-[#7F7F7F] text-center text-sm">Tap on the button to invite your friends</div>
-                <div className="text-white bg-[#FF403A] px-4 h-10 rounded-lg flex items-center flex-shrink-0 justify-center w-full">
+                <button 
+                    className="text-white bg-[#FF403A] px-4 h-10 rounded-lg flex items-center flex-shrink-0 justify-center w-full"
+                    onClick={handleInvite}
+                >
                     Invite frens
-                </div>
+                </button>
             </div>
         </div>
     )
